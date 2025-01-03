1099949fe115e1cc286b63e94861fc1075e7ed2c
Lahiru075 <sanjeewalahiru057@gmail.com>
2025-01-03 16:53:13 +0530

    third commit

--- src/Example.java
+++ src/Example.java
@@ -1,5 +1,12 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Example example = new Example();
+        example.print();
+    }
+
+    public void print() {
+        System.out.println("Hi");
     }
 }
